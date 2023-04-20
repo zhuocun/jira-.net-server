5b7b57048bf7bceb3d84500b95433a854024bb8a
zhuocun <zhuocun.tang@outlook.com>
2023-04-20 14:23:49 +1000

    style: removed unused code

--- Models/User.cs
+++ Models/User.cs
@@ -3,18 +3,10 @@ using System.ComponentModel.DataAnnotations;
 public class User
 {
     [Key]
-    public string _id { get; set; }
-    public string username { get; set; }
-    public string password { get; set; }
-    public string email { get; set; }
-    public List<String> likedProjects { get; set; }
+    public string _id { get; set; } = null!;
+    public string username { get; set; } = null!;
+    public string password { get; set; } = null!;
+    public string email { get; set; } = null!;
+    public List<String> likedProjects { get; set; } = new List<String>();
 
-    public User(string id, string username, string password, string email)
-    {
-        this._id = id;
-        this.username = username;
-        this.password = password;
-        this.email = email;
-        this.likedProjects = new List<String>();
-    }
 }
