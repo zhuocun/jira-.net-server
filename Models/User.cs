96b634821e2194eb3418a89a7a46bb3260e31dc8
zhuocun <zhuocun.tang@outlook.com>
2023-04-20 14:58:29 +1000

    feat: used mongo decorator

--- Models/User.cs
+++ Models/User.cs
@@ -1,12 +1,21 @@
-using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 public class User
 {
-    [Key]
-    public string _id { get; set; } = null!;
-    public string username { get; set; } = null!;
-    public string password { get; set; } = null!;
-    public string email { get; set; } = null!;
-    public List<String> likedProjects { get; set; } = new List<String>();
+    [BsonId]
+    [BsonElement("_id")]
+    public string Id { get; set; } = null!;
 
+    [BsonElement("username")]
+    public string Username { get; set; } = null!;
+
+    [BsonElement("password")]
+    public string Password { get; set; } = null!;
+
+    [BsonElement("email")]
+    public string Email { get; set; } = null!;
+
+    [BsonElement("likedProjects")]
+    public List<string> LikedProjects { get; set; } = new List<string>();
 }
